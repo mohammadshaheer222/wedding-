a24196a20cab67bd5a5b02251feb9bd12711fdce
mohammad shaheer <shaheercse2022@gmail.com>
2024-01-18 22:10:19 +0530

    video added

--- src/Components/Video.jsx
+++ src/Components/Video.jsx
@@ -3,10 +3,10 @@ import video1 from "../fotos";
 const Video = () => {
   return (
     <>
-      <div className="w-full h-full relative my-5">
-        <div className="absolute top-0 h-full flex justify-center items-end">
+      <div className="w-screen h-full relative my-5">
+        <div className="absolute top-0 w-screen h-full flex justify-center items-end">
           <Marquee>
-            <div className="flex">
+            <div className="flex overflow-hidden">
               <h1 className="video-h1">WEDDING</h1>
               <h1 className="video-h1">DAY</h1>
               <h1 className="video-h1">WEDDING</h1>
