daa5c9d5df5949cb213e494f16f75eaec83558c9
mohammad shaheer <shaheercse2022@gmail.com>
2024-01-18 22:02:00 +0530

    video added

--- src/Components/Video.jsx
+++ src/Components/Video.jsx
@@ -4,8 +4,8 @@ const Video = () => {
   return (
     <>
       <div className="w-full h-full relative my-5">
-        <div className="h-full flex justify-center items-end">
-          <Marquee autoFill="true" className="absolute top-0 w-full">
+        <div className="absolute top-0 h-full flex justify-center items-end">
+          <Marquee autoFill="true">
             <div className="flex">
               <h1 className="video-h1">WEDDING</h1>
               <h1 className="video-h1">DAY</h1>
