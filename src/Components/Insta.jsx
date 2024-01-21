a3225e0edc7e3f804829d15183e3da3b39dbf27a
mohammad shaheer <shaheercse2022@gmail.com>
2024-01-21 21:10:30 +0530

    font changed

--- src/Components/Insta.jsx
+++ src/Components/Insta.jsx
@@ -1,13 +1,14 @@
+import { carousel1, carousel4, carousel5 } from "../fotos";
 const Insta = () => {
   return (
     <>
       <div className="space-y-5">
         <h1 className="text-5xl md:text-6xl text-center">Follow us on Instagram</h1>
         <div className="w-screen flex ">
-          <a href="https://www.instagram.com/p/C2LASOCv1we/?img_index=1"><img className="h-full w-full" src="/src/assets/images/carousel/carousel1.jpg" alt="" /></a>
-          <a href="https://www.instagram.com/p/C2LASOCv1we/?img_index=1"><img className="h-full w-full" src="/src/assets/images/carousel/carousel4.jpg" alt="" /></a>
-          <a href="https://www.instagram.com/p/C2LASOCv1we/?img_index=1"><img className="h-full w-full" src="/src/assets/images/carousel/carousel5.jpg" alt="" /></a>
-          <a href="https://www.instagram.com/p/C2LASOCv1we/?img_index=1"><img className="h-full w-full" src="/src/assets/images/carousel/carousel1.jpg" alt="" /></a>
+          <a href="https://www.instagram.com/p/C2LASOCv1we/?img_index=1"><img className="h-full w-full" src={carousel1} alt="" /></a>
+          <a href="https://www.instagram.com/p/C2LASOCv1we/?img_index=1"><img className="h-full w-full" src={carousel4} alt="" /></a>
+          <a href="https://www.instagram.com/p/C2LASOCv1we/?img_index=1"><img className="h-full w-full" src={carousel5} alt="" /></a>
+          <a href="https://www.instagram.com/p/C2LASOCv1we/?img_index=1"><img className="h-full w-full" src={carousel1} alt="" /></a>
           
         </div>
       </div>
